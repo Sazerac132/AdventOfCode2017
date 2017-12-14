edda5f7b446d418025ab1e5af68167b8de36708f
Thomas McDevitt <thomas.mcdevitt@youview.com>
2017-12-14 11:46:15 +0000

    Completed day 14 of Advent of Code challenge.

--- day10/index.js
+++ day10/index.js
@@ -3,13 +3,15 @@
 let input = '88,88,211,106,141,1,78,254,2,111,77,255,90,0,54,205';
 let skip, position, totalLength = 256; // skip and position on global scope
 
-console.log('Part 1 answer:', hash1(input));
-console.log('Empty String:', hash2('')); // should be a2582a3a0e66e6e86e3812dcb672a272
-console.log('AoC 2017:', hash2('AoC 2017')); // should be 33efeb34ea91902bb2f59c9920caa6cd
-console.log('1,2,3:', hash2('1,2,3')); // should be 3efbe78a8d82f29979031a4aa0b16a9d
-console.log('1,2,4:', hash2('1,2,4')); // should be 63960835bcdc130f0b66d7ff4f6a5a8e
+const test = function() {
+    console.log('Part 1 answer:', hash1(input));
+    console.log('Empty String:', hash2('')); // should be a2582a3a0e66e6e86e3812dcb672a272
+    console.log('AoC 2017:', hash2('AoC 2017')); // should be 33efeb34ea91902bb2f59c9920caa6cd
+    console.log('1,2,3:', hash2('1,2,3')); // should be 3efbe78a8d82f29979031a4aa0b16a9d
+    console.log('1,2,4:', hash2('1,2,4')); // should be 63960835bcdc130f0b66d7ff4f6a5a8e
 
-console.log('Part 2 answer:', hash2(input));
+    console.log('Part 2 answer:', hash2(input));
+};
 
 function hashIteration(input, string) {
 
@@ -81,5 +83,14 @@ function hash2(input) {
     );
   }
 
-  return x.map(y => y.toString(16)).join('');
+  return x.map(decimalToHex).join('');
 }
+
+function decimalToHex(d) {
+  let hex = Number(d).toString(16);
+
+  if (hex.length < 2) return '0' + hex;
+  return hex;
+}
+
+module.exports = hash2;
